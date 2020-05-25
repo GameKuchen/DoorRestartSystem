725d805749d431034bbbabd8f4bbd2a5a3ae6ddd
elvarogaming <elvarogaming@gmail.com>
2020-05-25 22:43:04 +0200

    Code Cleanup

---
+++ DoorRestartSystem/EventHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using EXILED;
+using MEC;
+using Mirror;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorRestartSystem
+{
+    public class EventHandlers
+    {
+        public Plugin plugin;
+        public EventHandlers(Plugin plugin) => this.plugin = plugin;
+
+        public bool TeslasDisabled = false;
+        public List<CoroutineHandle> Coroutines = new List<CoroutineHandle>();
+
+        public void OnRoundStart()
+        {
+            foreach (CoroutineHandle handle in Coroutines)
+                Timing.KillCoroutines(handle);
+            TeslasDisabled = false;
+        }
+
+        public void OnRoundEnd()
+        {
+            foreach (CoroutineHandle handle in Coroutines)
+                Timing.KillCoroutines(handle);
+        }
+
+        public void OnWaitingForPlayers()
+        {
+            foreach (CoroutineHandle handle in Coroutines)
+                Timing.KillCoroutines(handle);
+            TeslasDisabled = false;
+        }
+
+        public void OnTriggerTesla(ref TriggerTeslaEvent ev)
+        {
+            if (TeslasDisabled)
+                ev.Triggerable = false;
+        }
+    }
+}
---
+++ DoorRestartSystem/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EXILED;
+using EXILED.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorRestartSystem
+{
+	public static class Extensions
+	{
+		
+
+		public static void Broadcast(this ReferenceHub rh, uint time, string message) =>
+			rh.GetComponent<Broadcast>()
+				.TargetAddElement(rh.scp079PlayerScript.connectionToClient, message, time, false);
+
+		
+}
+}

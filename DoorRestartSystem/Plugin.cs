bc725ec1dc162ca133f2e353990f42986fabee83
elvarogaming <elvarogaming@gmail.com>
2020-05-26 16:09:52 +0200

    Final Commit for Version 1.0

--- DoorRestartSystem/Plugin.cs
+++ DoorRestartSystem/Plugin.cs
@@ -51,8 +51,8 @@ namespace DoorRestartSystem
             InitialDelay = Config.GetFloat("drs_initial_delay", 120f);
             DurationMin = Config.GetFloat("drs_dur_min", 5f);
             DurationMax = Config.GetFloat("drs_dur_max", 10);
-            DelayMin = Config.GetInt("drs_delay_min", 180);
-            DelayMax = Config.GetInt("drs_delay_max", 300);
+            DelayMin = Config.GetInt("drs_delay_min", 300);
+            DelayMax = Config.GetInt("drs_delay_max", 500);
            
         }
         public override void OnDisable()

0b489d12456889eb9fb727e6d61a220d2f8d8e90
GameKuchen <49527417+GameKuchen@users.noreply.github.com>
2021-01-13 00:54:34 +0100

    Added new config options

--- DoorRestartSystem/Config.cs
+++ DoorRestartSystem/Config.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.ComponentModel;
 using Exiled.API.Interfaces;
 
@@ -24,14 +24,17 @@ namespace DoorRestartSystem
         [Description("The The Maxiumum Delay before the next the Lockdown")]
         public float DelayMax { get; set; } = 500f;
 
-        [Description("DoorRestartSencent")]
-        public string DoorSentence { get; set; } = "WARNING . DOOR SOFTWARE REPAIR IN t minus 20 seconds.";
+        [Description("The sentence it trasnmits via Cassie before the System gets restarted")]
+        public string DoorSentence { get; set; } = "pitch_0.2 .g4 . .g4 pitch_1 door control system pitch_0.25 .g1 pitch_0.9 malfunction pitch_1 . initializing repair";
+
+        [Description("Enable 3 . 2 . 1 announcement")]
+        public bool countdown { get; set; } = true;
 
         [Description("The time between the sentence and the 3 . 2 . 1 announcement")]
         public float timebtweensntnstart { get; set; } = 17f;
 
         [Description("DoorAfterRestartSencent")]
-        public string DoorAfterSentence { get; set; } = "DOOR SOFTWARE REPAIR COMPLETE";
+        public string DoorAfterSentence { get; set; } = "DOOR CONTROL SYSTEM REPAIR COMPLETE";
 
        
 

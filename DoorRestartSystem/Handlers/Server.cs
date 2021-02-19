e2ce4a08d71c22f7abd0054f33506f1f8eac50d0
GameKuchen <49527417+GameKuchen@users.noreply.github.com>
2021-02-19 18:41:09 +0100

    Fixed code formatting and the spawnchance

--- DoorRestartSystem/Handlers/Server.cs
+++ DoorRestartSystem/Handlers/Server.cs
@@ -7,12 +7,10 @@ namespace DoorRestartSystem.Handlers
 {
     internal sealed class Server
     {
-        private readonly Doorestartsystem plugin;
-        public Server(Doorestartsystem plugin) => this.plugin = plugin;
-
-       
-
+        private readonly DoorRestartsystem plugin;
+        public Server(DoorRestartsystem plugin) => this.plugin = plugin;
         public List<CoroutineHandle> Coroutines = new List<CoroutineHandle>();
+
         public void OnRoundStarted()
         {
             Log.Info("Reached OnROundstarted");
@@ -20,23 +18,22 @@ namespace DoorRestartSystem.Handlers
                 Timing.KillCoroutines(handle);
 
             int y = plugin.Gen.Next(100);
-            if (y < plugin.Config.spawnchance)
+            if (y <= plugin.Config.Spawnchance)
             {            
             Coroutines.Add(Timing.RunCoroutine(plugin.RunBlackoutTimer()));
             } 
         }
+
         public void OnRoundEnd(RoundEndedEventArgs ev)
         {
             foreach (CoroutineHandle handle in Coroutines)
                 Timing.KillCoroutines(handle);
         }
+
         public void OnWaitingForPlayers()
         {
             foreach (CoroutineHandle handle in Coroutines)
                 Timing.KillCoroutines(handle);
-        }
-        
+        }   
     }
-
-    
 }

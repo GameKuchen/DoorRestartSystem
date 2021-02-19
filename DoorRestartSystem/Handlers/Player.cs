37eef282a900373e4304cf9ea2b54c73fa142f99
GameKuchen <49527417+GameKuchen@users.noreply.github.com>
2021-02-19 18:42:01 +0100

    Fixed formatting

--- DoorRestartSystem/Handlers/Player.cs
+++ DoorRestartSystem/Handlers/Player.cs
@@ -4,16 +4,19 @@ namespace DoorRestartSystem.Handlers
 {
     class Player
     {
-        private readonly Doorestartsystem plugin;
-        public Player(Doorestartsystem plugin) => this.plugin = plugin;
-
-        
-
+        private readonly DoorRestartsystem plugin;
+        public Player(DoorRestartsystem plugin) => this.plugin = plugin;
         public bool TeslasDisabled = false;
+
         public void OnTriggerTesla(TriggeringTeslaEventArgs ev)
         {
             if (TeslasDisabled)
                 ev.IsTriggerable = false;
+        }
+
+        public void onWarheadStart(StartingEventArgs ev)
+        {
+            DoorRestartsystem.softlockDoors();
         }
     }
 }

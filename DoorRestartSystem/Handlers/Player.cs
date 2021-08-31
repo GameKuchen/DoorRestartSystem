3d64bcede04028a82ef6950897d544042b0c1c06
GameKuchen <49527417+GameKuchen@users.noreply.github.com>
2021-09-01 00:16:31 +0200

    Update to Exiled 3.0

--- DoorRestartSystem/Handlers/Player.cs
+++ DoorRestartSystem/Handlers/Player.cs
@@ -2,21 +2,16 @@ using Exiled.Events.EventArgs;
 
 namespace DoorRestartSystem.Handlers
 {
-    class Player
+    internal class Player
     {
-        private readonly DoorRestartSystem plugin;
-        public Player(DoorRestartSystem plugin) => this.plugin = plugin;
-        public bool TeslasDisabled = false;
+        private readonly DoorRestartSystem _plugin;
+        public Player(DoorRestartSystem plugin) => _plugin = plugin;
+        private bool TeslasDisabled = false;
 
         public void OnTriggerTesla(TriggeringTeslaEventArgs ev)
         {
             if (TeslasDisabled)
                 ev.IsTriggerable = false;
-        }
-
-        public void onWarheadStart(StartingEventArgs ev)
-        {
-            DoorRestartSystem.softlockDoors();
         }
     }
 }

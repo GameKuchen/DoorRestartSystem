2c4c637d5099041c737f54ba6da0659d62fb1c74
GameKuchen <49527417+GameKuchen@users.noreply.github.com>
2021-09-01 00:13:34 +0200

    Update to Exiled 3.0

--- DoorRestartSystem/Doorestartsystem.cs
+++ DoorRestartSystem/Doorestartsystem.cs
@@ -2,107 +2,96 @@ using System;
 using System.Collections.Generic;
 using Exiled.API.Enums;
 using Exiled.API.Features;
-using Exiled.API.Extensions;
 using Interactables.Interobjects.DoorUtils;
 using MEC;
 
 namespace DoorRestartSystem
-{
-
+{ 
     public class DoorRestartSystem : Plugin<Config>
     {
         public override string Author => "GameKuchen";
         public override string Name => "DoorRestartSystem";
         public override string Prefix => "DRS";
-        public override Version Version => new Version(2, 7, 0);
-        public override Version RequiredExiledVersion => new Version(2, 1, 30);
-        public Random Gen = new Random();
+        public override Version Version => new Version(3, 0, 0);
+        public override Version RequiredExiledVersion => new Version(3, 0, 0);
+        public Random Gen { get; } = new Random();
         public static DoorRestartSystem Singleton;
-        private Handlers.Server server;
-        private Handlers.Player player;
-        public NineTailedFoxAnnouncer Respawn;
+        private Handlers.Server _server;
+        private Handlers.Player _player;
         public static bool TimerOn = true;
-        System.Random random = new System.Random();
+        private System.Random Random { get; }= new System.Random();
         public override PluginPriority Priority => PluginPriority.Medium;
 
         public override void OnEnabled()
         {
             Singleton = this;
-            registerEvents();
+            RegisterEvents();
             base.OnEnabled();
         }
 
         public override void OnDisabled()
         {
-            Timing.KillCoroutines(server.Coroutine);
-            unRegisterEvents();
+            Timing.KillCoroutines(_server.Coroutine);
+            UnRegisterEvents();
             base.OnDisabled();
         }
 
-        public static void softlockDoors()
+        private void RegisterEvents()
         {
-            foreach (DoorVariant door in Map.Doors)
-            {
-                door.ServerChangeLock(DoorLockReason.Warhead, true);
-            }
+            _server = new Handlers.Server(this);
+            _player = new Handlers.Player(this);
+            Exiled.Events.Handlers.Server.RoundStarted += _server.OnRoundStarted;
+            Exiled.Events.Handlers.Server.RoundEnded += _server.OnRoundEnd;
+            Exiled.Events.Handlers.Server.WaitingForPlayers += _server.OnWaitingForPlayers;
+            Exiled.Events.Handlers.Player.TriggeringTesla += _player.OnTriggerTesla;
         }
 
-        private void registerEvents()
+        private void UnRegisterEvents()
         {
-            server = new Handlers.Server(this);
-            player = new Handlers.Player(this);
-            Exiled.Events.Handlers.Server.RoundStarted += server.OnRoundStarted;
-            Exiled.Events.Handlers.Server.RoundEnded += server.OnRoundEnd;
-            Exiled.Events.Handlers.Server.WaitingForPlayers += server.OnWaitingForPlayers;
-            Exiled.Events.Handlers.Player.TriggeringTesla += player.OnTriggerTesla;
-        }
+            Exiled.Events.Handlers.Server.RoundStarted -= _server.OnRoundStarted;
+            Exiled.Events.Handlers.Server.RoundEnded -= _server.OnRoundEnd;
+            Exiled.Events.Handlers.Server.WaitingForPlayers -= _server.OnWaitingForPlayers;
+            Exiled.Events.Handlers.Player.TriggeringTesla -= _player.OnTriggerTesla;
 
-        private void unRegisterEvents()
-        {
-            Exiled.Events.Handlers.Server.RoundStarted -= server.OnRoundStarted;
-            Exiled.Events.Handlers.Server.RoundEnded -= server.OnRoundEnd;
-            Exiled.Events.Handlers.Server.WaitingForPlayers -= server.OnWaitingForPlayers;
-            Exiled.Events.Handlers.Player.TriggeringTesla -= player.OnTriggerTesla;
-
-            server = null;
-            player = null;
+            _server = null;
+            _player = null;
         }
 
         public IEnumerator<float> RunBlackoutTimer()
         {
             yield return Timing.WaitForSeconds(Config.InitialDelay);
-            yield return Timing.WaitForSeconds((float)random.NextDouble() * (Config.DelayMax - Config.DelayMin) + Config.DelayMin);
+            yield return Timing.WaitForSeconds((float)Random.NextDouble() * (Config.DelayMax - Config.DelayMin) + Config.DelayMin);
             for (; ; )
             {
                 yield return Timing.WaitUntilTrue(() => !(Warhead.IsDetonated || Warhead.IsInProgress));
-                Cassie.Message(Config.DoorSentence, false, true);
+                Cassie.Message(Config.DoorSentence);
 
                 TimerOn = true;
                 yield return Timing.WaitForSeconds(Config.TimeBetweenSentenceAndStart);
                 if (Config.Countdown)
                 {
-                    Cassie.Message("3 . 2 . 1", false, true);
+                    Cassie.Message("3 . 2 . 1");
                     yield return Timing.WaitForSeconds(3f);
                 }
 
-                float BlackoutDur = (float)(random.NextDouble() * (Config.DurationMax - Config.DurationMin) + Config.DurationMin);
+                float blackoutDur = (float)(Random.NextDouble() * (Config.DurationMax - Config.DurationMin) + Config.DurationMin);
 
 
-                foreach (DoorVariant door in Map.Doors)
-                {
-                   if(door.Type() != DoorType.NukeSurface)
+                foreach (var door in Map.Doors)
+                { 
+                    if(door.Type != DoorType.NukeSurface) 
                     {
-                        door.NetworkTargetState = false;
-                        door.ServerChangeLock(DoorLockReason.SpecialDoorFeature, true);
+                        door.Open = false;
+                        door.Base.ServerChangeLock(DoorLockReason.AdminCommand, true);
                     }
                 }
-                yield return Timing.WaitForSeconds(BlackoutDur);
-                foreach (DoorVariant door in Map.Doors)
+                yield return Timing.WaitForSeconds(blackoutDur);
+                foreach (var door in Map.Doors)
                 {
-                    door.ServerChangeLock(DoorLockReason.SpecialDoorFeature, false);
+                    door.Base.ServerChangeLock(DoorLockReason.AdminCommand, false);
                 }
-                Cassie.Message(Config.DoorAfterSentence, false, true);
-                yield return Timing.WaitForSeconds((float)random.NextDouble() * (Config.DelayMax - Config.DelayMin) + Config.DelayMin);
+                Cassie.Message(Config.DoorAfterSentence);
+                yield return Timing.WaitForSeconds((float)Random.NextDouble() * (Config.DelayMax - Config.DelayMin) + Config.DelayMin);
                 TimerOn = false;
             }
         }

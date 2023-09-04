c1b97dcd7fb870246d61558c98ddc7c219048d40
Yannis Winkler <49527417+GameKuchen@users.noreply.github.com>
2023-09-04 22:25:15 +0200

    Fixes

--- DoorRestartSystem/DoorRestartSystem.cs
+++ DoorRestartSystem/DoorRestartSystem.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Exiled.API.Enums;
 using Exiled.API.Features;
+using Exiled.API.Features.Doors;
 using Exiled.Loader;
 using MEC;
 using Server = DoorRestartSystem.Handlers.Server;
@@ -14,7 +15,7 @@ namespace DoorRestartSystem
         public override string Author => "GameKuchen & iomatix";
         public override string Name => "DoorRestartSystem";
         public override string Prefix => "DRS";
-        public override Version Version => new Version(4, 0, 0);
+        public override Version Version => new Version(4, 1, 0);
         public override Version RequiredExiledVersion => new Version(7, 0, 0);
         private Server _server;
         public override PluginPriority Priority => PluginPriority.Medium;

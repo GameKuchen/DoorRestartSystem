09b59b2f1a2bc0a835c0f09d62483a92c8c03bc5
Yannis Winkler <49527417+GameKuchen@users.noreply.github.com>
2024-08-18 18:07:14 +0200

    Revert "Update to work with 8.9.11 not 9.0.0"
    This reverts commit 899cf1e4ae487b022b880961aa328d5e95c03418.
    
    revert

--- DoorRestartSystem/DoorRestartSystem.cs
+++ DoorRestartSystem/DoorRestartSystem.cs
@@ -16,7 +16,7 @@ namespace DoorRestartSystem
         public override string Name => "DoorRestartSystem";
         public override string Prefix => "DRS";
         public override Version Version => new Version(6, 1, 0);
-        public override Version RequiredExiledVersion => new Version(8, 9, 11);
+        public override Version RequiredExiledVersion => new Version(9, 0, 0);
         private Server _server;
         public override PluginPriority Priority => PluginPriority.Medium;
 
